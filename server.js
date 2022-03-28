3298a65d3c2642cf0781317dada3718a4b86eb7f
Viken-zip <assiviikman@gmail.com>
2022-03-28 12:18:04 +0300

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const expressLayout = require('express-ejs-layout');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const server = express();
+const indexRouter = require('./controller/index');
+
+//view engine
+server.set('view engine', 'ejs');
+server.set('layout', 'layouts/layout');
+server.use(expressLayout);
+//server.use(cookieParser);
+server.use(express.static('public'));
+server.use('/', indexRouter);
+
+server.listen(process.env.PORTS, ()=>{
+    console.log('server connected');
+});
